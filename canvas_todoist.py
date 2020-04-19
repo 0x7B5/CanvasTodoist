9e2275e90d6528b368ce7a05063c8ee4be09296d
Vlad Munteanu <vladcmunteanu@gmail.com>
2020-04-19 01:36:35 -0400

    Added getting canvas assignments.

--- canvas_todoist.py
+++ canvas_todoist.py
@@ -1,8 +1,41 @@
 import todoist
 from canvasapi import Canvas
+from dateutil import parser
 
-def main(): 
-    print("Main")
 
-if __name__ == '__main__': 
+def getCanvasToken():
+    canvas_token = open("Tokens/canvas.txt", "r")
+    return canvas_token.read().strip("\n")
+
+
+def getTodoistToken():
+    canvas_token = open("Tokens/todoist.txt", "r")
+    return canvas_token.read().strip("\n")
+
+
+def getCanvasEvents():
+    API_URL = "https://canvas.vt.edu"
+    canvas = Canvas(API_URL, getCanvasToken())
+    events = canvas.get_upcoming_events()
+
+    events_dict = {}
+
+    for item in events:
+        if 'assignment' in item:
+            date = parser.parse(item['assignment']['due_at'])
+            formated_date = "{}-{}-{}".format(date.year, date.month, date.day)
+            title = item['title']
+
+            if formated_date in events_dict:
+                events_dict[formated_date].append(title)
+            else:
+                events_dict[formated_date] = [title]
+    return events_dict
+
+
+def main():
+    print(getCanvasEvents())
+
+
+if __name__ == '__main__':
     main()
